5b1f281dc8aca604b3f0cef064f8a2460fae20ab
Min Jia <jia00018@algonquinlive.com>
2018-11-09 15:12:12 -0500

    deleted movie and food java files in master

--- app/src/main/java/com/example/minjia/finalproject/CBCNewsActivity.java
+++ app/src/main/java/com/example/minjia/finalproject/CBCNewsActivity.java
@@ -2,15 +2,25 @@ package com.example.minjia.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class CBCNewsActivity extends Activity {
-    static final String ACTIVITY_NAME = "CBCNews Reader Activity";
+    private static final String ACTIVITY_NAME = "CBCNews Reader Activity";
+    private ProgressBar progressBar;
+    private ImageView CBCNewsImage;
+    private TextView newsTitle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cbcnews);
+
+        progressBar = findViewById(R.id.progressBar);
+        CBCNewsImage=findViewById(R.id.CBCNewsImage);
+
 
 
     }
