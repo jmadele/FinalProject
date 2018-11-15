7b290e37d91708a13425406c53fa545a372c658d
TaoLin <tao00015@algonquinlive.com>
2018-11-15 01:32:28 -0500

    Nov15

--- app/src/main/java/com/example/minjia/finalproject/FoodNutrition.java
+++ app/src/main/java/com/example/minjia/finalproject/FoodNutrition.java
@@ -1,16 +1,27 @@
 package com.example.minjia.finalproject;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class FoodNutrition extends AppCompatActivity {
+public class FoodNutrition extends Activity {
 
+    private static final String ACTIVITY_NAME = "Food Nutrition Activity";
+    private ProgressBar FoodNutritionProgressBar;
+    private ImageView FoodNutritionImage;
+    private TextView FoodNutritionView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_nutrition);
 
+        FoodNutritionProgressBar = (ProgressBar) findViewById(R.id.FoodNutritionProgressBar);
+        FoodNutritionImage=(ImageView)findViewById(R.id.FoodNutritionImage);
+        FoodNutritionView=(TextView)findViewById(R.id.FoodNutritionView);
 
-        ////
     }
 }
