acffac199dc0ac5583f07a470e7fa5f3d1ee0fa7
CST2335 <jing.chen@hotmail.ca>
2018-11-15 10:07:11 -0500

    Nov 15 - bus

--- app/src/main/java/com/example/minjia/finalproject/OCTranspo.java
+++ app/src/main/java/com/example/minjia/finalproject/OCTranspo.java
@@ -2,14 +2,64 @@ package com.example.minjia.finalproject;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+/**
+ * Main Activity for the OCTranspo route information App.
+ * User can enter a bus station number (4 digits) which is saved to the database and displayed on
+ * screen in a list.
+ * User can then select a route from the list and get the routes available from that station/stop.
+ *
+ * @author xinji on 2018-11-12
+ */
+
 
 public class OCTranspo extends Activity {
 
+
+    /**
+     * Called on activity start. Provides content view, loads resources and sets button functions.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_octranspo);
+
+
+
     }
 
 }
