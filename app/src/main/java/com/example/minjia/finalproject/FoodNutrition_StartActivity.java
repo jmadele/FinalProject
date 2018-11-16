ce027280ef37c318b5c0534d66ade243791f3170
TaoLin <tao00015@algonquinlive.com>
2018-11-16 15:05:37 -0500

    Merge branch 'master' of https://github.com/jmadele/FinalProject into branchT
    # Conflicts:
    #	app/src/main/java/com/example/minjia/finalproject/FoodNutrition.java
    #	app/src/main/java/com/example/minjia/finalproject/MainActivity.java
    #	app/src/main/res/layout/activity_food_nutrition.xml

--- app/src/main/java/com/example/minjia/finalproject/FoodNutrition_StartActivity.java
+++ app/src/main/java/com/example/minjia/finalproject/FoodNutrition_StartActivity.java
@@ -2,6 +2,7 @@ package com.example.minjia.finalproject;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.support.design.widget.Snackbar;
+import android.widget.Toolbar;
 
 public class FoodNutrition_StartActivity extends AppCompatActivity {
 
@@ -24,17 +26,19 @@ public class FoodNutrition_StartActivity extends AppCompatActivity {
     private ListView foodListView;
     private Button searchButton;
     private Dialog foodNutritionDialog;
+    //private Toolbar foodNutritionToolBar;
 
 
     /**
      * Called on activity start. Provides content view, loads resources and sets button functions.
+     *
      * @param savedInstanceState
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_nutrition_start_activity);
-        Log.i(ACTIVITY_NAME, "In onCreate()");
+        //Log.i(ACTIVITY_NAME, "In onCreate()");
 
         foodNutritionProgressBar = findViewById(R.id.foodNutritionProgressBar);
         foodNutritionImage = findViewById(R.id.foodNutritionImage);
@@ -42,65 +46,45 @@ public class FoodNutrition_StartActivity extends AppCompatActivity {
         foodListView = findViewById(R.id.foodListView);
         foodNutritionProgressBar = findViewById(R.id.progress);
         foodNutritionImage = findViewById(R.id.foodNutritionImage);
+        //foodNutritionToolBar = findViewById(R.id.foodNutritionToolBar);
 
         /**
          * Create a button for searches
          */
 
-        Toast.makeText(this,"searching", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"searching", Toast.LENGTH_SHORT).show();
 
-        searchButton.setOnClickListener(e ->{
-                    Snackbar.make(e, "submitting", Snackbar.LENGTH_LONG).show();
+        //   searchButton.setOnClickListener(e -> {
+        //     Snackbar.make(e, "submitting", Snackbar.LENGTH_LONG).show();
+        //});
 
-    }
-            //public void onClick(View v) {
-                //Intent intent = new Intent(FoodNutrition_StartActivity.this,
-                //        FoodNutrition_ListItemsActivity.class);
-                //startActivityForResult(intent, 50);
-            //}
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "In searching", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    @Override
-    public void onActivityResult(int requestCode,int responseCode, Intent data){
-        //Lab3.11
-        if (requestCode == 50) {
-            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
-        }
-        if (responseCode == Activity.RESULT_OK) {
-            String messagePassed = data.getStringExtra("Response");
-            CharSequence text = "ListItemActivity passed: My information to share";
-            Toast toast = Toast.makeText(this, text+messagePassed,
-                    Toast.LENGTH_LONG); //this is the ListActivity
-            toast.show();
-        }
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.i(ACTIVITY_NAME, "In onResume()");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(ACTIVITY_NAME, "In onStart()");
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.i(ACTIVITY_NAME, "In onPause()");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.i(ACTIVITY_NAME, "In onStop()");
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
+            //public void onClick(View v) {
+                //Intent intent = new Intent(FoodNutrition_StartActivity.this,
+                //       FoodNutrition_ListItemsActivity.class);
+              //  startActivityForResult(intent, 50);
+            //}
+
+    //@Override
+    //public void onActivityResult(int requestCode,int responseCode, Intent data){
+        //Lab3.11
+        //if (requestCode == 50) {
+        //    Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+       // }
+        //if (responseCode == Activity.RESULT_OK) {
+          //  String messagePassed = data.getStringExtra("Response");
+         //   CharSequence text = "ListItemActivity passed: My information to share";
+        //    Toast toast = Toast.makeText(this, text+messagePassed,
+       //             Toast.LENGTH_LONG);
+      //      toast.show();
+     //   }
+    //}
