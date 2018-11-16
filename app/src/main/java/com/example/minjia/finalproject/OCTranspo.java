d983424e8f9e4477ab039ac8b672c67b8d2e88a8
CST2335 <jing.chen@hotmail.ca>
2018-11-16 12:43:53 -0500

    finished milestone 1

--- app/src/main/java/com/example/minjia/finalproject/OCTranspo.java
+++ app/src/main/java/com/example/minjia/finalproject/OCTranspo.java
@@ -13,6 +13,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.Movie;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,11 +27,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -46,7 +50,15 @@ import java.util.LinkedList;
  */
 
 
-public class OCTranspo extends Activity {
+public class OCTranspo extends AppCompatActivity {
+    private static final String ACTIVITY_NAME = "OCTransport Activity";
+    private ProgressBar progressBar;
+    private ImageView busImage;
+    private EditText stationInput;
+    private ListView stationsList;
+    private Button submitButton;
+
+    private Dialog stationDialog;
 
 
     /**
@@ -58,8 +70,26 @@ public class OCTranspo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_octranspo);
 
+        stationsList = findViewById(R.id.stationsList);
 
+        submitButton = findViewById(R.id.submitButton);
+
+        stationInput = findViewById(R.id.stationInput);
+
+        progressBar = findViewById(R.id.progress);
+
+        busImage = findViewById(R.id.busImg);
+
+
+        Toast.makeText(this,"searching", Toast.LENGTH_SHORT).show();
+
+        submitButton.setOnClickListener(e ->{
+        Snackbar.make(e, "submitting", Snackbar.LENGTH_LONG).show();
+
+
+    });
 
     }
+
 
 }
