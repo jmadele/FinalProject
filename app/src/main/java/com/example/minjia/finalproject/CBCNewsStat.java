e5be3e8f288a8df6d564d91f979ba069d93604db
Min Jia <jia00018@algonquinlive.com>
2018-12-05 18:49:26 -0500

    CBC news: enabled save feature

--- app/src/main/java/com/example/minjia/finalproject/CBCNewsStat.java
+++ app/src/main/java/com/example/minjia/finalproject/CBCNewsStat.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 
+/**
+ * this class holds the information called when clicking on the save button in CBC News Content
+ * and when the emulator is using phone instead of tablet
+ */
 public class CBCNewsStat extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,25 +17,29 @@ public class CBCNewsStat extends Activity {
         String description = getIntent().getExtras().getString("desc");
         String pubDate =getIntent().getExtras().getString("pubDate");
         String author =getIntent().getExtras().getString("author");
+
         int position = getIntent().getExtras().getInt("position");
         long id =getIntent().getExtras().getLong("id");
+
+        String str = getIntent().getExtras().getString("TITLE");
 
         Bundle bundle = new Bundle();
         CBCNewsFragment fragment = new CBCNewsFragment();
         fragment.isTablet=false;
-        bundle.putString("desc",description);
-        bundle.putString("title",title);
-        bundle.putString("pubDate",pubDate);
-        bundle.putString("author",author);
+//        bundle.putString("desc",description);
+//        bundle.putString("title",title);
+//        bundle.putString("pubDate",pubDate);
+//        bundle.putString("author",author);
         bundle.putLong("id",id);
         bundle.putInt("position",position);
+        bundle.putString ("TITLE",str);
         bundle.putBoolean("isTablet",false);
 
         fragment.setArguments(bundle);
         FragmentTransaction ft = getFragmentManager().beginTransaction();
+        //update information in fragment
         ft.replace(R.id.fragment_frame, fragment);
         ft.addToBackStack(null);
         ft.commit();
     }
-
 }
